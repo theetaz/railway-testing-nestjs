f1fdecf88ab4edd3fc551c43a558bf88eb2d9f6d
Nipun Theekshana <theekshana2@gmail.com>
2024-08-03 20:42:06 +0200

    feat: add simple hello world endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { ConfigModule } from '@nestjs/config';
+import { HelloController } from './hello/hello.controller';
+import { HelloModule } from './hello/hello.module';
 
 @Module({
   imports: [
-    HealthModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    HealthModule,
+    HelloModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, HelloController],
   providers: [AppService],
 })
 export class AppModule {}
